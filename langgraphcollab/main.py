ed87dc1bd4b92fb76c51b594ce20f22a69818a66
Augusto Hagemeier <augustolhagemeier@gmail.com>
2025-12-14 16:38:50 -0300

    Avanço collab

---
+++ langgraphcollab/main.py
@@ -0,0 +1,25 @@
+from mathcollab import simple_workflow,SimpleAgentState
+
+def main():
+  print("===Multiagente matemático===")
+  query = input("Digite uma expressão matemática (ex: 2=2*3):")
+
+  state = SimpleAgentState(
+    original_query= query,
+    plan= None,
+    current_task_idx = 0,
+    intermediate_results = {},
+    final_response = None,
+    error = None,
+    current_task_description= None,
+    current_specialist_type= None,
+    current_task_id= None,
+    specialist_result= None
+  )
+
+  result = simple_workflow.invoke(state)
+  print("\n=== Resposta Final ===")
+  print(result.get("final_response","nenhuma resposta gerada"))
+
+if __name__ == "__main__":
+  main()
